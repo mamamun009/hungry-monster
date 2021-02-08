30a0d3eaa5ce13e2fb7c6cde20ddb73e1b9f9b71
Abs Mamun <M A Mamun>
2021-02-08 23:22:40 +0600

    update3

--- script.js
+++ script.js
@@ -1,30 +1,36 @@
+let foodName = " ";
 //search btn
 document.getElementById('search').addEventListener('click', () => {
     document.getElementById('infoDiv').style.display = 'none';
     document.getElementById('mainDiv').innerHTML = "";
-    // document.getElementById('recommend').style.display = 'none';
-    // document.getElementById('searchDiv').style.display = 'block';
-    const foodName = document.getElementById('foodName').value;
+    foodName = document.getElementById('foodName').value;
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${foodName}`)
         .then(response => response.json())
         .then(data => {
-            for (let i = 0; i < data.meals.length; i++) {
-                const eachData = data.meals[i];
-                const mainDiv = document.getElementById('mainDiv');
-                const subDiv = document.createElement('div');
-                const divElements = `<div onclick="clickOnDiv(${i},'${foodName}')" class="grid-item">
-                                    <img src="${eachData.strMealThumb}">
-                                    <h5>${eachData.strMeal}</h5></div>`
-                subDiv.innerHTML = divElements;
-                mainDiv.appendChild(subDiv);
-                // console.log(subDiv);
+            if (data.meals === null || data.meals == ""){
+                document.getElementById('error').style.display = 'block';
+            }
+            else {
+                for (let i = 0; i < data.meals.length; i++) {
+                    const eachData = data.meals[i];
+                    const mainDiv = document.getElementById('mainDiv');
+                    const subDiv = document.createElement('div');
+                    const divElements = `<div onclick="clickOnDiv(${i},'${foodName}')" class="grid-item">
+                                        <img src="${eachData.strMealThumb}">
+                                        <h5>${eachData.strMeal}</h5></div>`
+                    subDiv.innerHTML = divElements;
+                    mainDiv.appendChild(subDiv);
+                }
             }
 
-        })
+
+        }).catch(() => {
+            document.getElementById('serverError').style.display = 'block';
+        });
 })
 let clickOnDiv = (id, foodName) => {
     document.getElementById('infoDiv').style.display = 'block';
-    window.scrollTo(0, 150);
+    window.scrollTo(0, 60);
     document.getElementById('ul').innerHTML = "";
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${foodName}`)
         .then(response => response.json())
@@ -40,7 +46,11 @@ let clickOnDiv = (id, foodName) => {
             }
             document.getElementById('headTitle').innerText = data.meals[id].strMeal;
             document.getElementById('recipe').innerText = data.meals[id].strInstructions;
-        })
+        }).catch(() => {
+            document.getElementById('serverError').style.display = 'block';
+            document.getElementById('infoDiv').style.display = 'none';
+            document.getElementById('mainDiv').style.display = 'none';
+        });
 }
 document.getElementById('hideDetails').addEventListener('click', () => {
     document.getElementById('infoDiv').style.display = 'none';
